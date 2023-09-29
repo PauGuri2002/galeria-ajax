cc0dff6b3c1e556b74651515df092504c3ad92a2
Pau <pauguvi@gmail.com>
2023-09-30 01:08:14 +0200

    Exercici 1 done

--- Exercici 1/main.js
+++ Exercici 1/main.js
@@ -1,30 +1,70 @@
 const key = "85b0f5e9c40241aeaaad41c8899ddc66";
 const genres = "indie";
-const page_size = 30;
+const game_page_size = 30;
+const dev_page_size = 10;
 const order = "-metacritic";
-const list = document.getElementById('list');
+const game_list = document.getElementById('game-list');
+const dev_list = document.getElementById('dev-list');
+const bottom_container = document.getElementById('bottom-container');
 const details = document.getElementById('details');
-var currentPage = 1;
+var game_page = 1;
+var dev_page = 1;
 
-//window.addEventListener('load', populateList);
+window.addEventListener('load', populateGameList);
+window.addEventListener('load', populateDevList);
 
-function populateList() {
-    list.innerHTML = "";
+function populateGameList() {
+    game_list.innerHTML = "";
     details.innerHTML = "";
 
-    fetch("https://api.rawg.io/api/games?key=" + key + "&genres=" + genres + "&page_size=" + page_size + "&page=" + currentPage + "&ordering=" + order)
+    fetch("https://api.rawg.io/api/games?key=" + key + "&genres=" + genres + "&page_size=" + game_page_size + "&page=" + game_page + "&ordering=" + order)
         .then(response => response.json())
         .then(data => {
             data.results.forEach(game => {
                 let score = game.metacritic;
                 let color = score < 50 ? "red" : score < 75 ? "orange" : "green";
 
-                let li = document.createElement('li');
-                li.addEventListener('click', (event) => populateDetails(event));
-                li.dataset.id = game.id;
-                li.innerHTML = `<p>${game.name}</p><div style="background-color: ${color}; color: white;">&nbsp;${score}&nbsp;</div>`;
-                list.appendChild(li);
+                let div = document.createElement('div');
+                div.addEventListener('click', (event) => populateDetails(event));
+                div.classList.add('game');
+                div.dataset.id = game.id;
+                div.innerHTML = `<p>${game.name}</p><div style="background-color: ${color};">&nbsp;${score}&nbsp;</div>`;
+                game_list.appendChild(div);
             });
+        })
+}
+
+var isPopulating = false;
+function populateDevList() {
+    if (isPopulating) {
+        return;
+    }
+    isPopulating = true;
+    bottom_container.removeEventListener('scroll', checkScroll);
+
+    fetch("https://api.rawg.io/api/developers?key=" + key + "&page_size=" + dev_page_size + "&page=" + dev_page)
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(dev => {
+                let div = document.createElement('div');
+                div.classList.add('dev');
+                div.style.backgroundImage = `url(${dev.image_background})`;
+
+                let dev_overlay = document.createElement('div');
+                dev_overlay.classList.add('dev-overlay');
+
+                let dev_name = document.createElement('p');
+                dev_name.classList.add('dev-name');
+                dev_name.innerHTML = dev.name;
+
+                div.appendChild(dev_overlay);
+                div.appendChild(dev_name);
+                dev_list.appendChild(div);
+            });
+
+            bottom_container.addEventListener('scroll', checkScroll);
+            dev_page++;
+            isPopulating = false;
         })
 }
 
@@ -42,20 +82,70 @@ function populateDetails(event) {
     fetch("https://api.rawg.io/api/games/" + id + "?key=" + key)
         .then(response => response.json())
         .then(data => {
-            details.innerHTML += `<h1>${data.name}</h1>`;
-            details.innerHTML += `<img src="${data.background_image}" alt="Cover art for ${data.name}">`;
-            details.innerHTML += `<p>${data.description}</p>`;
-            details.innerHTML += `<p>Released: ${data.released}</p>`;
-            details.innerHTML += `<p>Metacritic score: ${data.metacritic}</p>`;
-            details.innerHTML += `<p>Developed by: ${data.developers.map(developer => developer.name).join(", ")}</p>`;
-            details.innerHTML += `<p>Platforms: ${data.platforms.map(platform => platform.platform.name).join(", ")}</p>`;
+
+            // BANNER
+            let banner = document.createElement('div');
+            banner.classList.add('banner');
+            banner.style.backgroundImage = `url(${data.background_image})`;
+
+            let banner_overlay = document.createElement('div');
+            banner_overlay.classList.add('banner-overlay');
+
+            let banner_name = document.createElement('h2');
+            banner_name.classList.add('name');
+            banner_name.innerHTML = data.name;
+
+            let banner_developer = document.createElement('p');
+            banner_developer.classList.add('developer');
+            banner_developer.innerHTML = data.developers.map(developer => developer.name).join(", ");
+
+            let metacritic = document.createElement('div');
+            metacritic.classList.add('metacritic');
+            metacritic.innerHTML = data.metacritic;
+
+            banner.appendChild(banner_overlay);
+            banner.appendChild(banner_name);
+            banner.appendChild(banner_developer);
+            banner.appendChild(metacritic);
+            details.appendChild(banner);
+
+            // RATING
+            let rating = document.createElement('div');
+            rating.classList.add('rating');
+
+            let stars = document.createElement('span');
+            stars.classList.add('stars');
+            for (let i = 1; i <= 5; i++) {
+                stars.innerHTML += i <= Math.round(data.rating) ? "★" : "☆";
+            }
+
+            let rating_count = document.createElement('span');
+            rating_count.classList.add('rating-count');
+            rating_count.innerHTML = " · " + data.ratings_count + " ratings";
+
+            rating.appendChild(stars);
+            rating.appendChild(rating_count);
+            details.appendChild(rating);
+
+            // DESCRIPTION
+            let description = document.createElement('div');
+            description.classList.add('description');
+            description.innerHTML = data.description;
+            details.appendChild(description);
         })
 }
 
+function checkScroll(event) {
+    let element = event.currentTarget;
+    if (element.scrollWidth - element.scrollLeft === element.clientWidth) {
+        populateDevList();
+    }
+}
+
 function changePage(offset) {
-    let newPage = currentPage + offset;
-    if (newPage >= 1) {
-        currentPage = newPage;
+    let new_page = game_page + offset;
+    if (new_page >= 1) {
+        game_page = new_page;
         populateList();
     }
 }
