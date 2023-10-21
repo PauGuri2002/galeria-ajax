e1fa438bb6f898d09b442d90fbd737e7e3c64d27
Pau <pauguvi@gmail.com>
2023-10-21 10:56:09 +0200

    exercici 4 done

--- Exercici 4/main.js
+++ Exercici 4/main.js
@@ -3,11 +3,12 @@ const fetchUrls = {
     alwaysOpen: "https://citmalumnes.upc.es/~davids/awug1_2324/05_maps/benzineres/llista24hs.php",
     details: "https://citmalumnes.upc.es/~davids/awug1_2324/05_maps/benzineres/benzinera.php"
 }
+const toggleCooldown = 200;
 
 class GasStationMap {
     gasStations = [];
 
-    constructor(startLat, startLong, zoom, mapContainerId, detailsContainerId) {
+    constructor(startLat, startLong, zoom, listContainerId, mapContainerId, detailsContainerId) {
         let position = new google.maps.LatLng(startLat, startLong);
         let options = {
             zoom: zoom,
@@ -15,12 +16,26 @@ class GasStationMap {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         this.map = new google.maps.Map(document.getElementById(mapContainerId), options);
+
+        this.listContainer = document.getElementById(listContainerId);
+        this.detailsContainer = document.getElementById(detailsContainerId);
     }
 
     loadStations(fetchUrl) {
         fetch(fetchUrl)
             .then(response => response.json())
             .then(data => {
+                // clear existing markers
+                this.gasStations.forEach(station => {
+                    station.marker.setMap(null)
+                });
+                this.gasStations = [];
+                this.listContainer.innerHTML = "";
+
+                // hide details container
+                this.detailsContainer.classList.remove("visible");
+
+                // add new markers
                 data.benzineres.forEach(station => {
                     let stationInstance = new GasStation(station.id, station.lat, station.lon, station.nom);
                     this.gasStations.push(stationInstance);
@@ -30,6 +45,14 @@ class GasStationMap {
                         let gasStation = this.getGasStationById(station.id);
                         this.renderDetails(gasStation);
                     });
+
+                    let listItem = document.createElement("li");
+                    listItem.innerHTML = station.nom;
+                    listItem.addEventListener('click', () => {
+                        let gasStation = this.getGasStationById(station.id);
+                        this.renderDetails(gasStation);
+                    });
+                    this.listContainer.appendChild(listItem);
                 });
             })
             .catch(err => { console.error(err); })
@@ -40,7 +63,36 @@ class GasStationMap {
     }
 
     renderDetails(gasStation) {
-        // TODO: Render gas station details
+        this.map.panTo(gasStation.marker.getPosition());
+        this.map.setZoom(16);
+
+        gasStation.getDetails(details => {
+            this.detailsContainer.innerHTML = "";
+
+            let h2 = document.createElement("h2");
+            h2.innerHTML = gasStation.name;
+            this.detailsContainer.appendChild(h2);
+
+            let address = document.createElement("div");
+            address.classList.add("address");
+            address.innerHTML = details.adreca;
+            this.detailsContainer.appendChild(address);
+
+            let timetable = document.createElement("div");
+            timetable.classList.add("timetable");
+            timetable.innerHTML = details.horari;
+            this.detailsContainer.appendChild(timetable);
+
+            let contact = document.createElement("div");
+            contact.classList.add("contact");
+            let contactDetails = [];
+            if (details.email.length > 0) contactDetails.push("Email: " + details.email);
+            if (details.telefon.length > 0) contactDetails.push("Telèfon: " + details.telefon);
+            contact.innerHTML = contactDetails.join("<br />");
+            this.detailsContainer.appendChild(contact);
+
+            this.detailsContainer.classList.add("visible");
+        });
     }
 }
 
@@ -92,7 +144,22 @@ class GasStation {
     }
 }
 
+// instantiate and setup map
+
+let isToggling = false;
 window.addEventListener('load', () => {
-    let map = new GasStationMap(41.558976707885435317955675107, 2.0238975952806974206832272891, 13, "map-container", "details-container");
+    let map = new GasStationMap(41.558976707885435317955675107, 2.0238975952806974206832272891, 13, "list", "map", "details-container");
     map.loadStations(fetchUrls.all);
+
+    let gasStationToggle = document.querySelector(".toggle");
+    gasStationToggle.addEventListener('click', () => {
+        if (isToggling) return;
+        isToggling = true;
+
+        gasStationToggle.classList.toggle("on");
+        let fetchUrl = gasStationToggle.classList.contains("on") ? fetchUrls.alwaysOpen : fetchUrls.all;
+        map.loadStations(fetchUrl);
+
+        setTimeout(() => { isToggling = false; }, toggleCooldown);
+    })
 })
